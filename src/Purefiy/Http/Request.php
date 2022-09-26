58e3d7f51f5951a05185e59606e35e3d991afb59
ashkanfekri <ashkanfekridev@gmail.com>
2022-09-27 02:20:46 +0330

    Update Request.php

--- src/Purefiy/Http/Request.php
+++ src/Purefiy/Http/Request.php
@@ -1,6 +1,16 @@
 <?php
 namespace Pureify\Http;
+
 class Request
 {
+    public static function uri(): string
+    {
+        return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+    }
+
+    public static function method(): static
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
 
 }
